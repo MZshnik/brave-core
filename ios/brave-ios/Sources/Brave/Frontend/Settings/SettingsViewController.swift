a8686c8899f6a894fc0838f14740c6a783aff8a1
Eli Hini <8249954+EliHini@users.noreply.github.com>
2026-03-31 14:08:44 -0700

    fix navigation bar not restoring when auth lock is dismissed on failure
    Signed-off-by: Eli Hini <8249954+EliHini@users.noreply.github.com>

--- ios/brave-ios/Sources/Brave/Frontend/Settings/SettingsViewController.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Settings/SettingsViewController.swift
@@ -214,8 +214,6 @@ class SettingsViewController: TableViewController {
     super.viewWillAppear(animated)
     // Reset dev options access count
     aboutHeaderTapCount = 0
-    // Hide toolbar in case it was enabled by a child controller
-    navigationController?.setToolbarHidden(true, animated: animated)
   }
 
   private func displayRewardsDebugMenu() {
@@ -1316,7 +1314,7 @@ class SettingsViewController: TableViewController {
               let autofillDataManager = braveCore.defaultWebViewConfiguration.autofillDataManager
             {
               let viewModel = ManagePasswordsViewModel(autofillDataManager: autofillDataManager)
-              let controller = UIHostingController(
+              let controller = AutofillSettingsHostingController(
                 rootView:
                   ManagePasswordsView(viewModel: viewModel)
                   .environment(
@@ -1329,7 +1327,6 @@ class SettingsViewController: TableViewController {
               )
 
               navigationController?.pushViewController(controller, animated: true)
-              navigationController?.setToolbarHidden(false, animated: true)
             } else {
               let loginsPasswordsViewController = LoginListViewController(
                 passwordAPI: passwordAPI,
