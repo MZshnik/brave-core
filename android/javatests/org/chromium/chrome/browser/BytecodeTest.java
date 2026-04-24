7461700d70c123ec6cf5a98db517c2c86261fb29
Artem Samoilenko <artem@brave.com>
2026-04-24 07:12:57 -0400

    [cr149][Android] Remove Page Summary adaptive toolbar button
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/f71a274e4251ab0af4309311f1872115cce080df
    
    commit f71a274e4251ab0af4309311f1872115cce080df
    Author: Bryan Song <bryansong@google.com>
    Date:   Tue Apr 21 16:45:11 2026 -0700
    
        Remove Page Summary adaptive toolbar button
    
        There's an adaptive toolbar button that was created to test Chrome's AI
        integrations. It is no longer used. Therefore, this CL removes the
        button and associated services.
    
        Bug: 500481129
        Change-Id: If52a300da2af950d5303e867546ee369e1042114
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7762974
        Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
        Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
        Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
        Commit-Queue: Bryan Song <bryansong@google.com>
        Cr-Commit-Position: refs/heads/main@{#1618547}

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -2324,7 +2324,6 @@ public class BytecodeTest {
                         Runnable.class,
                         Supplier.class,
                         Supplier.class,
-                        Supplier.class,
                         MonotonicObservableSupplier.class,
                         GlicToolbarButtonController.GlicButtonDelegate.class,
                         Supplier.class,
